b8f2ef8c6c874dd4f355ed774599bedbd0bff351
Prioxe <36611417+Prioxe@users.noreply.github.com>
2019-09-18 15:17:33 -0300

    54. Componente Menu
    O menu está feito!

--- todo-app/frontend/src/main/app.jsx
+++ todo-app/frontend/src/main/app.jsx
@@ -4,10 +4,12 @@
  import React from 'react'
  import Todo from '../todo/todo'
  import About from '../about/about'
+ import Menu from '../template/menu'
  
 
  export default props => (
      <div className='container'>         
+         <Menu />
          <Todo />
          <About />
      </div>
